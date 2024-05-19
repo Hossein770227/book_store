5f8522c0a7e4ee40ddbe1e01e578a4122cd9ced2
hossein <hossein.h.a770227@gmail.com>
2024-05-19 18:29:38 +0330

    add myuser models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class MyUser(AbstractBaseUser):
+    phone_number =  models.CharField(verbose_name=_('phone number'), max_length=11, unique=True)
+    full_name= models.CharField(verbose_name=_('full name'), max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_admin =models.BooleanField(default=False)
+
+
+
+    USERNAME_FIELD ='phone_number'
+    REQUIRED_FIELDS =['full_name']
+    
+    def __str__(self):
+        return self.full_name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    @property
+    def is_superuser(self):
+        return True
--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,6 @@ STATICFIELS_DIRS =os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# account config 
+AUTH_USER_MODEL = 'accounts.MyUser'
