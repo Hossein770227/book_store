b9e3e7c95519ef31121f0015ba763b3826f9e154
hossein <hossein.h.a770227@gmail.com>
2024-05-20 16:38:46 +0330

    add Book models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Book(models.Model):
+    title =models.CharField(_("title"), max_length=100)
+    description = models.TextField(_("description"))
+    author = models.CharField(_("author"),max_length=100)
+    price = models.PositiveIntegerField(_("price"))
+    date_time_created = models.DateTimeField(_("date time created"), auto_now_add=True)
+    date_time_modified = models.DateTimeField(_("date time modified"), auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
