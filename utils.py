a1b12369a60d7fcec3de1b4ebc5169a48ba7a0e3
hossein <hossein.h.a770227@gmail.com>
2024-05-21 14:53:25 +0330

    change otp code

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ from django.conf import settings
 
 
 def send_otp_code(phone_number, code):
-    url = 'https://api.limosms.com/api/sendcode'
-    myobj = {'Mobile' :f'{phone_number}',
-    'Footer' : f'{code}کد تایید شما'}
-    x = requests.post(url, json = myobj, headers = {"ApiKey":settings.API_KEY})
+    url = 'https://api.limosms.com/api/sendpatternmessage'
+    message = [f"{code}"]
+    myobj = {'OtpId' :'593',
+    'ReplaceToken' : message,
+    'MobileNumber' : f'{phone_number}'}
+    x = requests.post(url, json = myobj, headers = {"ApiKey": settings.API_KEY})
     print(x.text)
