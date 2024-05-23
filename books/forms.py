ed7f451fd4149199dc6059eee5f1d9ee01231f05
hossein <hossein.h.a770227@gmail.com>
2024-05-23 14:16:24 +0330

    add comment view

--- books/forms.py
+++ books/forms.py
@@ -4,4 +4,10 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
     class Meta:
+        model = Comment
         fields = ['email', 'text']
+        widgets = {
+            # 'email': forms.Textarea(attrs={'placeholder':'for example emailname@gmail.com'}),
+            'text': forms.Textarea(attrs={'placeholder':'write your comment'}),
+        }
+    
--- books/views.py
+++ books/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render , get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Book, Comment
+from .forms import CommentForm
 
 class BookListView(ListView):
     model =Book
     template_name= 'books/book_list.html'
     context_object_name = 'books'
+
+    ordering = ['-date_time_created']
 
 
 # class BookDetailView(DetailView):
@@ -17,4 +20,14 @@ class BookListView(ListView):
 def book_detail_view(request, pk):
     book = get_object_or_404(Book,pk=pk )
     comment =Comment.objects.filter(active=True)
-    return render(request,'books/book_detail.html',{'book':book,"comments":comment} )
+    if request.method =='POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.book = book
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+    return render(request,'books/book_detail.html',{'book':book,"comments":comment, 'comment_form': comment_form} )
